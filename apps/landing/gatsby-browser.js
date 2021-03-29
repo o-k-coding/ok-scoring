54e291125fda36a2f7918ca6ea77738c53c0dabc
Michael O'Keefe <okeefem355@gmail.com>
2021-03-28 23:30:51 -0400

    Creating landing page for ok-scoring using gatsby

--- apps/landing/gatsby-browser.js
+++ apps/landing/gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// custom typefaces
+// import "typeface-montserrat"
+// import "typeface-merriweather"
+// normalize CSS across browsers
+// custom CSS styles
+// import "./src/style.css"
--- apps/landing/src/pages/index.tsx
+++ apps/landing/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
 
 import styles from './index.module.scss';
+import PrivacyPolicy from './privacy-policy/PrivacyPolicy';
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
 
-export function Index() {
+export function Index({ data }) {
   /*
    * Replace the elements below with your own.
    *
@@ -13,93 +15,48 @@ export function Index() {
    */
   return (
     <div className={styles.page}>
-      <header className="flex">
-        <Logo width="75" height="75" />
-        <h1>Welcome to landing!</h1>
+      <header>
+        <div>
+          <Img fixed={data.splashImage.childImageSharp.fixed} alt='ok scoring logo' />
+        </div>
+        <h1>OK Scoring</h1>
       </header>
       <main>
-        <h2>Resources &amp; Tools</h2>
-        <p>Thank you for using and showing some ♥ for Nx.</p>
-        <div className="flex github-star-container">
-          <a
-            href="https://github.com/nrwl/nx"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            If you like Nx, please give it a star:
-            <div className="github-star-badge">
-              <img src={star} className="material-icons" alt="" />
-              Star
-            </div>
-          </a>
+        <p style={{ margin: '50px' }}>
+          Check out our app on the IOS App Store for all of your game scoring needs!
+        </p>
+        <div>
+          <Img fixed={data.appHomeImage.childImageSharp.fixed} alt='ok scoring app home screen' />
         </div>
-        <p>Here are some links to help you get started.</p>
-        <ul className="resources">
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://connect.nrwl.io/app/courses/nx-workspaces/intro"
-            >
-              Nx video course
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/getting-started/what-is-nx"
-            >
-              Nx video tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a
-              className="resource flex"
-              href="https://nx.dev/react/tutorial/01-create-application"
-            >
-              Interactive tutorial
-            </a>
-          </li>
-          <li className="col-span-2">
-            <a className="resource flex" href="https://connect.nrwl.io/">
-              <img
-                height="36"
-                alt="Nrwl Connect"
-                src="https://connect.nrwl.io/assets/img/CONNECT_ColorIcon.png"
-              />
-              <span className="gutter-left">Nrwl Connect</span>
-            </a>
-          </li>
-        </ul>
-        <h2>Next Steps</h2>
-        <p>Here are some things you can do with Nx.</p>
-        <details open>
-          <summary>Add UI library</summary>
-          <pre>{`# Generate UI lib
-nx g @nrwl/react:lib ui
-
-# Add a component
-nx g @nrwl/react:component xyz --project ui`}</pre>
-        </details>
-        <details>
-          <summary>View dependency graph</summary>
-          <pre>{`nx dep-graph`}</pre>
-        </details>
-        <details>
-          <summary>Run affected commands</summary>
-          <pre>{`# see what's been affected by changes
-nx affected:dep-graph
-
-# run tests for current changes
-nx affected:test
-
-# run e2e tests for current changes
-nx affected:e2e
-`}</pre>
-        </details>
+        <PrivacyPolicy />
       </main>
     </div>
   );
 }
 
 export default Index;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    splashImage: file(relativePath: { eq: "splash.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+
+    appHomeImage: file(relativePath: { eq: "app-home.png" }) {
+      childImageSharp {
+        fixed {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
